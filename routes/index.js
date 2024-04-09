9233a4e065fb750786049b102b48e9932cfe40e4
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-04-09 09:45:24 -0400

    mongo db connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const { MongoClient } = require('mongodb')
+const client = new MongoClient('mongodb://0.0.0.0:27017')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'sajith',name:"hai this sajith" });
 });
+router.post("/signup",function(req,res){
+ var detail= req.body)
+ // Connect to database 
+client.connect()
+.then(() => {(client.db("user").collection('usercol').insertOne(detail))})
+.catch(error => console.log('Failed to connect', error))
+ 
 
+})
 module.exports = router;
